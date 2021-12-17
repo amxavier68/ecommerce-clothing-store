3a0c58a291f67482bdf8f3ab857f3a79d1831916
Anthony Xavier <anthony-xavier@live.com>
2021-12-17 12:09:17 +1000

    Added minor css changes

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -50,8 +50,10 @@ class SignIn extends Component {
             label='password'
             required
           />
-          <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          <div className='buttons'>
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          </div>
         </form>
 
       </div>
